498923435db33bcefd58b80f8edd8214fea7668e
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-24 14:27:06 -0300

    Adicionado alert dialog para ocuapção e para países visitados

--- app/src/main/java/com/example/diegoTeixeira7/cadastro/Controller/MainActivity.java
+++ app/src/main/java/com/example/diegoTeixeira7/cadastro/Controller/MainActivity.java
@@ -11,32 +11,36 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btnGenre;
+    private Button btnOccupation;
+    private Button btnCountry;
+    private Button btnPets;
 
-    private CharSequence[] select_genre;
-    private CharSequence[] select_occupation;
-    private CharSequence[] select_country;
-    private CharSequence[] select_pets;
+    private CharSequence[] selectGenre;
+    private CharSequence[] selectOccupation;
+    private CharSequence[] selectCountry;
+    private CharSequence[] selectPets;
+
+    private boolean[] selectCountryItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.genreBtn);
+        btnGenre = findViewById(R.id.genreBtn);
 
-        select_genre = new CharSequence[] {
-            "Masculino", "Feminino", "Outro (Por favor especifique)", "Prefiro não dizer"
+        selectGenre = new CharSequence[] {
+            "Masculino", "Feminino", "Outro", "Prefiro não dizer"
         };
 
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnGenre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this)
                         .setTitle("Seu gênero");
 
-                builder.setSingleChoiceItems(select_genre, 0, new DialogInterface.OnClickListener() {
+                builder.setSingleChoiceItems(selectGenre, 0, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
@@ -61,16 +65,89 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        select_occupation = new CharSequence[] {
-                "Engenheiro", "Feminino", "França", "Nenhum"
+        btnOccupation = findViewById(R.id.occupationBtn);
+
+        selectOccupation = new CharSequence[] {
+                "Estudante", "Engenheiro(a)", "Médico(a)", "Arquieto(a)", "Eletricista", "Diarista", "Professor", "Outro"
         };
 
-        select_country = new CharSequence[] {
-                "EUA", "Rússia", "Pedreiro", "Prefiro não dizer"
+        btnOccupation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this)
+                        .setTitle("Sua Ocupação");
+
+                builder.setSingleChoiceItems(selectOccupation, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        btnCountry = findViewById(R.id.countryBtn);
+
+        selectCountry = new CharSequence[] {
+                "EUA", "Rússia", "Argentina", "Canadá", "Irlanda", "Outro(s)", "Nenhum"
         };
 
-        select_pets = new CharSequence[] {
-                "Cachorro", "Gato", "Pássaro", "Nenhum"
+        selectCountryItems = new boolean[]{
+                false, false, false, false, false, false, false
+        };
+
+        btnCountry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this)
+                        .setTitle("Selecione os países que você já visitou");
+
+                builder.setMultiChoiceItems(selectCountry, selectCountryItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    }
+                });
+
+                builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+
+        btnPets = findViewById(R.id.petsBtn);
+
+        selectPets = new CharSequence[] {
+                "Cachorro", "Gato", "Pássaro", "Outro", "Nenhum"
         };
     }
 }
